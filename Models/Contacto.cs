6c97142472c85997bd64bd948954169512fab1ed
Abrahansito <huamancarlosabraham@gmail.com>
2025-03-09 17:28:02 -0500

    Code

--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace apptienda.Models
 {
     public class Contacto
     {
-        [NotNull]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string? Nombre { get; set; }
         [NotNull]
         public string? Email { get; set; } 
